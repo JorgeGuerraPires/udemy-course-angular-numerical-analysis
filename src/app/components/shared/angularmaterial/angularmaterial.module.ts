8b4d5deda2714ad44be3d82d50319ab2677c33c8
Jorge Pires <jorgeguerrapires@yahoo.com.br>
2021-11-24 08:01:05 -0300

    deploying module 4

--- src/app/components/shared/angularmaterial/angularmaterial.module.ts
+++ src/app/components/shared/angularmaterial/angularmaterial.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 //1. import the API from Angular Material Library
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 //All the angular material imports come here
 //2. import into our app
-const angularmaterial = [MatToolbarModule];
+const angularmaterial = [MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatDividerModule];
 
 
 
--- src/app/components/shared/header/header.component.spec.ts
+++ src/app/components/shared/header/header.component.spec.ts
@@ -1,25 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
+
+//Angular material components
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+
+
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let loader: HarnessLoader;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      imports: [MatMenuModule, MatIconModule, MatDividerModule, MatToolbarModule]
+
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load all menu harnesses', async () => {
+    const menues = await loader.getAllHarnesses(MatMenuHarness);
+    expect(menues.length).toBe(1);
+  });
+
 });
